7f34820343cc5d1c5ab1a472fe11d16efb2811ce
IngmarN <91394490+IngmarN@users.noreply.github.com>
2022-01-12 00:28:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scorer_1 = "Ruud Gullit"
+scorer_2 = "Marco van Basten"
+goal_0 = 32
+goal_1 = 54
+
+scorers = scorer_1 + " " +\
+    str(goal_0) + ", " + \
+    scorer_2 + " " + str(goal_1)
+
+report = (
+    f'{scorer_1} scored in the {goal_0}nd minute'"\n" f' {scorer_2} scored in the {goal_1}th minute')
+print(report)
+
+player = "Ronald Koeman"
+first_name = player[:6]
+last_name_len = len(player[7:])
+name_short = (f'{player[0]}. {player[7:]}')
+chant = ((first_name + "! ")*6)[:-1]
+good_chant = chant[-1] != " "
